edb09af4b6f27fad204404113a5b73b263cc2502
samutrujillo <samueltrujillorodrigue@gmail.com>
2022-01-05 19:08:42 -0500

    update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # disables authentication
 file_line { 'Declare identity file':
   path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/holberton',
+  line    => '    IdentityFile ~/.ssh/school',
   replace => true,
 }
 
